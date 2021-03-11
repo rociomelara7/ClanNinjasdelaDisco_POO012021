f7468a49df92f8b85834bc02187745db476dfd8e
MauriR511 <62717542+MauriR511@users.noreply.github.com>
2021-03-10 19:57:24 -0600

    Subiendo ejercicio

---
+++ PrimerPrograma/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace PrimerPrograma
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            int a = 0, b = 0;
+
+            Console.WriteLine("Enter a value for a: ");
+            a = Int32.Parse(Console.ReadLine());
+
+            Console.WriteLine("Enter a value for b: ");
+            b = Int32.Parse(Console.ReadLine());
+
+            Console.WriteLine($"Adding {a} + {b} equals {AddNumbers (a, b)} ");
+        }
+
+        public static int AddNumbers(int a, int b)
+        {
+            return a + b;
+        }
+    }
+}
